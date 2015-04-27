dc7d5a1549615bd1952678d3928537fd3b939944
Adrian Kemp <adrian.kemp@me.com>
2015-04-27 07:37:40 -0400

    Initial Commit

---
+++ AKPageIndicator/AKPageIndicator.h
@@ -0,0 +1,19 @@
+//
+//  AKPageIndicator.h
+//  AKPageIndicator
+//
+//  Created by Adrian Kemp on 2015-04-27.
+//  Copyright (c) 2015 Adrian Kemp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AKPageIndicator.
+FOUNDATION_EXPORT double AKPageIndicatorVersionNumber;
+
+//! Project version string for AKPageIndicator.
+FOUNDATION_EXPORT const unsigned char AKPageIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKPageIndicator/PublicHeader.h>
+
+
